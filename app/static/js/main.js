843f77c512039f10d86462cc3cf4a03282d28e47
Orzechowski <b.orzechowski@schattdecor.pl>
2022-08-11 15:50:37 +0200

    list of lists mobile done

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,21 +1,36 @@
 const toggleBurgerMenu = () => {
-    console.log('object');
-    const BurgerIcon = document.querySelector('.bars-icon')
-    const xMark = document.querySelector('.xmark-icon')
+    const burgerIcon = document.querySelector('.bars-icon')
+    const xMark = document.querySelector('.xmark-icon-menu')
     const menu = document.querySelector('.menu')
+    const arrow = document.querySelector('.right-arrow-icon')
 
-    menu.classList.toggle('show')
-    BurgerIcon.classList.toggle('hide')
+    menu.classList.toggle('show-menu')
+    burgerIcon.classList.toggle('hide')
     xMark.classList.toggle('hide')
+    arrow.classList.toggle('hide')
+}
+
+const toggleListMenu = () => {
+    const burgerIcon = document.querySelector('.bars-icon')
+    const xMark = document.querySelector('.xmark-icon-lists')
+    const aside = document.querySelector('.aside')
+    const arrow = document.querySelector('.right-arrow-icon')
+
+    aside.classList.toggle('show-aside')
+    burgerIcon.classList.toggle('hide')
+    xMark.classList.toggle('hide')
+    arrow.classList.toggle('hide')
 }
 
 
 
 
 const main = () => {
-    const icons = document.querySelectorAll('.menu-icon')
+    const menuIcons = document.querySelectorAll('.menu-icon')
+    const asideIcons = document.querySelectorAll('.aside-icon')
 
-    icons.forEach(icon => {icon.addEventListener('click', toggleBurgerMenu)})
+    menuIcons.forEach(icon => {icon.addEventListener('click', toggleBurgerMenu)})
+    asideIcons.forEach(icon => {icon.addEventListener('click', toggleListMenu)})
 }
 
 main()
