1454eeaca0e1adb4915a82e6723960087248ec1d
orzehhowski <filmyminecraftnayt@gmail.com>
2022-08-24 10:52:35 +0200

    first restful commit

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,14 +3,16 @@ from app.main import bp
 from flask import render_template, jsonify, request
 from flask_login import current_user, login_required
 from app.models import List, Task
+from flask_restful import Resource
 
 
-@bp.route('/')
-@bp.route('/index')
-@login_required
-def index():
-    lists = List.query.filter_by(author=current_user).order_by(-List.id)
-    return render_template('index.html', lists=lists, title='ToDo List')
+# @bp.route('/')
+# @bp.route('/index')
+class Index(Resource):
+    @login_required
+    def get(self):
+        lists = List.query.filter_by(author=current_user).order_by(-List.id)
+        return render_template('index.html', lists=lists, title='ToDo List')
 
 
 @bp.route('/add-list', methods=['POST'])
